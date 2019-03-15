479a40fad51ac8e24a28c91847d7c3c0b97e9749
Imran Bepari <imranbepari97@gmail.com>
2019-03-15 14:58:13 +0000

    starting being able to poll crime API

--- js/displayMap.js
+++ js/displayMap.js
@@ -9,3 +9,57 @@ var map = new L.Map("map", {
 //set up geocoder
 var geocoder = new L.Control.Geocoder();
 	geocoder.addTo(map);
+
+var heat;
+
+function addCrimeToHeat(data) {
+	console.log(data);
+	var heatData = [];
+	//data.forEach(function (d) {
+		
+	//});
+	
+	//heat = L.heatLayer(data, {radius: 12.5}).addTo(map);
+};
+
+//Get updated crime data 	
+map.on('moveend', function() { 
+	var bounds = map.getBounds();
+	var boundsString = bounds.getNorthEast().lat + "," + bounds.getNorthEast().lng + ":" + bounds.getNorthWest().lat + "," + bounds.getNorthWest().lng + ":" + bounds.getSouthWest().lat + "," + bounds.getSouthWest().lng + ":" + bounds.getSouthEast().lat + "," + bounds.getSouthEast().lng;
+	var requestString = "https://data.police.uk/api/crimes-street/all-crime?poly=" + boundsString;
+	 //console.log(map.getBounds());
+	
+	$.ajax({
+		url: requestString,
+		type: "GET",
+		success: function(result) {
+			addCrimeToHeat(result);
+		},
+		error: function(error) {
+			console.log("Too much data returned.");
+		}
+	});
+	
+
+});
+
+
+// add code for crime rate
+// create the control on the top left
+var crimeTick = L.control({position: 'topleft'});
+// what to do when it's added to the map 
+crimeTick.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'crimeRate');
+	//html for the control, in this case checkbox
+    div.innerHTML = '<form><input id="crimeRate" type="checkbox"/>Crime Rate Display</form>'; 
+    return div;
+};
+// actually add it
+crimeTick.addTo(map);
+// function that will be called when the checkbox is ticked
+function handleCrime() {
+	
+   alert("Clicked, checked = " + this.checked);
+}
+//add the handler so that the handleCrime function is called on click
+document.getElementById ("crimeRate").addEventListener ("click", handleCrime, false);
